ebab80f16f5185a09637363b18fedd13b376e276
Luis Felipe Klaus <duckbox@hotmail.com>
2021-10-19 16:14:26 -0300

    prisma client created at src/prisma AUS updated accordingly

--- node_heat/src/services/AuthenticateUserService.ts
+++ node_heat/src/services/AuthenticateUserService.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import prismaClient from "../prisma";
+import { sign } from 'jsonwebtoken';
 
 /**
  * Receive code(string)
@@ -44,7 +46,43 @@ class AuthenticateUserService {
             },
         });
 
-        return response.data;
+        // Check if user is already in database
+        const { login, id, avatar_url, name} = response.data
+
+        let user = await prismaClient.user.findFirst({
+            where: {
+                github_id: id,
+            },
+        });
+
+        if (!user) {
+            user = await prismaClient.user.create({
+                data: {
+                    github_id: id,
+                    login,
+                    avatar_url,
+                    name,
+                },
+            });
+        }
+
+        const token = sign(
+            {
+                user: {
+                    name: user.name,
+                    avatar_url: user.avatar_url,
+                    id: user.id,
+                }
+            },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: "1d"
+            }
+        
+        );
+
+        return { token, user };
     }
 }
 
