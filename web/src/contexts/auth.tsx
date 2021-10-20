087a1994c827d0f8b3a91f61419d4d008cd9ec6c
Luis Felipe Klaus <duckbox@hotmail.com>
2021-10-20 13:15:56 -0300

    Login box refactored. context/auth.tsx added

---
+++ web/src/contexts/auth.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import { createContext, ReactNode } from "react";
+
+import { api } from '../services/api';
+
+type User = {
+    id: string;
+    login: string;
+    avatar_url: string;
+}
+
+type AuthContextData = {
+    user: User | null;
+    signInUrl: string;
+}
+
+// Sintax to create and export a context in react using ts
+export const AuthContext = createContext({} as AuthContextData)
+
+type AuthProvider = {
+    children: ReactNode;
+}
+
+type AuthResponse = {
+    token: string;
+    user: {
+        id: string;
+        avatar_url: string;
+        name: string;
+        login: string;
+    }
+}
+
+export function AuthProvider(props: AuthProvider) {
+    const [user, setUser] = useState<User | null>(null)
+
+    const signInUrl = `https://github.com/login/oauth/authorize?scope=user&client_id=c3a108968c3848ceb58f&redirect_uri=http://localhost:3000`;
+
+    async function signIn(githubCode: string) {
+        const response = await api.post<AuthResponse>('authenticate', {
+            code: githubCode,
+        })
+
+        const { token, user } = response.data
+
+        localStorage.setItem('@dowhile:token', token)
+
+        setUser(user)
+    }
+    
+    useEffect(() => {
+        const url = window.location.href;
+        const hasGithubCode = url.includes('?code');
+
+        if (hasGithubCode) {
+            const [urlWithoutCode, githubCode] = url.split('?code=');
+        
+            // Clear url in order to hide provided code (empty params, empty url, urlWithoutCode)
+            window.history.pushState({}, '', urlWithoutCode);
+
+            signIn(githubCode)
+        }
+    }, [])
+
+    return (
+        <AuthContext.Provider value={{ signInUrl, user }}>
+            {props.children}
+        </AuthContext.Provider>
+    ); 
+}
