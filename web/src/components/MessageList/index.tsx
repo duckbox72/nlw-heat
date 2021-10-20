982d061e496af37ba923f36421ea2b5cc96d2ce1
Luis Felipe Klaus <duckbox@hotmail.com>
2021-10-19 22:53:30 -0300

    MessageList and styles implemented

--- web/src/components/MessageList/index.tsx
+++ web/src/components/MessageList/index.tsx
@@ -1,5 +1,45 @@
+import styles from './styles.module.scss';
+
+import logoImg from '../../assets/logo.svg';
+
 export function MessageList() {
     return (
-        <h1>Message List</h1>
+       <div className={styles.messageListWrapper}>
+           <img src={logoImg} alt="Do/While 2021" />
+
+           <ul className={styles.messageList}>
+               
+               <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/duckbox72.png" alt="Luis Felipe Klaus" />
+                        </div>
+                        <span>Luis Felipe Klaus</span>
+                    </div>
+               </li>
+               
+               <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/duckbox72.png" alt="Luis Felipe Klaus" />
+                        </div>
+                        <span>Luis Felipe Klaus</span>
+                    </div>
+               </li>
+               
+               <li className={styles.message}>
+                    <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamooo pra cima! 🔥🔥</p>
+                    <div className={styles.messageUser}>
+                        <div className={styles.userImage}>
+                            <img src="https://github.com/duckbox72.png" alt="Luis Felipe Klaus" />
+                        </div>
+                        <span>Luis Felipe Klaus</span>
+                    </div>
+               </li>
+               
+           </ul>
+       </div>
     )
 }
